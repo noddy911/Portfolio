2d9b9db4e42a1247ea96d2b8295a3ea0ceab34d6
noddy911 <navdep149@example.com>
2024-11-10 21:59:44 +0530

    initial-commit

---
+++ src/assets/index.js
@@ -0,0 +1,78 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import bill from "./bill.jpg"
+import larry from "./larry.jpg"
+import steve from "./steve.jpg"
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.png";
+import tsp from "./company/tsp.png"
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import afflux from "./company/afflux.png"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import stock from "./stock.jpg";
+import quiz from "./quiz.png";
+import wise from "./wise.jpeg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  tsp,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  bill,
+  larry,
+  steve,
+  reactjs,
+  tesla,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  afflux,
+  carrent,
+  jobit,
+  tripguide,
+  stock,
+  quiz,
+  wise,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,241 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    quiz,
+    wise,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    bill,
+    larry,
+    steve,
+    tsp,
+    afflux,
+    stock,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Fast",
+      icon: web,
+    },
+    {
+      title: "Responsive",
+      icon: mobile,
+    },
+    {
+      title: "Intuitive",
+      icon: backend,
+    },
+    {
+      title: "Dynamic",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Fullstack Web developer",
+      company_name: "Unified Mentor",
+      icon: afflux,
+      iconBg: "#383E56",
+      date: "Nov 2024 - Present",
+      points: [
+        "Built basic banking system with front-end of ejs and back-end with node-js.",
+        "Integrated it with payment methods.",
+        "Build the database in Mongo.db .",
+        "Recognise by team for completed the tasks within 30 days.",
+        
+      ],
+    },
+    {
+      title: "Frontend developer as a Freelancer",
+      company_name: "Kshree Furniture.",
+      icon: tsp,
+      iconBg: "#E6DEDD",
+      date: "March 2023 - April 2023",
+      points: [
+        
+        "Developing and maintaining website using html,css,js and other related technologies.",
+        "Created a furniture website for the client.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Success is a lousy teacher. It seduces smart people into thinking they can't lose.                 ",
+      name: "Bill Gates",
+      image: bill,
+    },
+    {
+      testimonial:
+        "Because the people who are crazy enough to think they can change the world are the ones who do.",
+      name: "Steve Jobs",
+      image: steve,
+    },
+    {
+      testimonial:
+        "If you're changing the world, you're working on important things. You're excited to get up in the morning. ",
+      name: "Larry Page",
+      image: larry,
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Wise India",
+      description:
+        "Web-based platform that allows users to vote there opinion of trending world affairs. Architected and developed a scalable web application using React.js, enabling real-time voting and opinion sharing on trending news topics Implemented user authentication and real-time database operations using Firebase, supporting over 1,000 concurrent users",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Firebase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Cloud Firestore",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: wise,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Quiz App",
+      description: "Quiz app built with React.js, Node.js, and MongoDB. Users upload custom questions. Features card-based interface showing one question at a time, with instant feedback. Demonstrates skills in creating responsive, user-friendly web apps with frontend-backend integration. Showcases full-stack development abilities in modern technologies.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Node.js",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: quiz,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Automated Stock Trading Bot",
+      description:"Developed Python stock trading bot using historical data analysis and machine learning. Implemented sentiment analysis of financial news with NLP to inform trading decisions. Backtested algorithm on 5 years of historical data. Achieved simulated return 15% higher than market indices.",
+      tags: [
+        {
+          name: "Python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "MachineLearning",
+          color: "green-text-gradient",
+        },
+        {
+          name: "MongoDB",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: stock,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
